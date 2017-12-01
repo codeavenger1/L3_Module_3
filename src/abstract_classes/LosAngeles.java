f7e106471a9a590860f8e3ae1f6153de6754f3cf
DavidAndBlake <League@AdministorsiMac.attlocal.net>
2017-11-30 17:00:59 -0800

    finished interffaces and abstracts and mod 3

---
+++ src/abstract_classes/LosAngeles.java
@@ -0,0 +1,16 @@
+package abstract_classes;
+
+public class LosAngeles extends City{
+
+	public LosAngeles(int population, double growthRate) {
+		super(population, growthRate);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	double getAnnualTaxes() {
+		// TODO Auto-generated method stub
+		return population*growthRate*((growthRate*0.5));
+	}
+
+}
